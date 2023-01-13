0d5580043027b4bf6ebbd990a251a081e0984aa2
saradeej <sara.djermoun@gmail.com>
2023-01-12 16:59:20 -0800

    update style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import "./App.css";
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import './App.css';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id){
+    setNotes(prevNotes =>{
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Notes App</p>
-      </header>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return ( 
+          <Note 
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+        />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
